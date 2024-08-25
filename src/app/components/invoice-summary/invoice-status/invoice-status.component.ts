c2efa3f25a73c9c6495fee1d9781dd58a40435f3
Santy <passiflora.freelance@gmail.com>
2024-08-25 14:01:03 -0500

    ♻️ Updating invoice-status components logic and data binding

--- src/app/components/invoice-summary/invoice-status/invoice-status.component.ts
+++ src/app/components/invoice-summary/invoice-status/invoice-status.component.ts
@@ -1,13 +1,17 @@
-import { Component, input } from '@angular/core';
-import { TStatus } from '../../../models/types/states.type';
+import { Component, computed, input } from '@angular/core';
+import { TStatusSlug } from '../../../models/types/states.type';
+import { IState } from '../../../models/interfaces/state.interface';
+import { States } from '../../../constants/states.constant';
 
 @Component({
   selector: 'app-invoice-status',
   standalone: true,
   imports: [],
   templateUrl: './invoice-status.component.html',
-  styleUrl: './invoice-status.component.scss'
+  styleUrl: './invoice-status.component.scss',
 })
 export class InvoiceStatusComponent {
-  status = input<TStatus>('Unpaid');
+  statusType = input<TStatusSlug>('unpaid');
+
+  status = computed<IState>(() => States[this.statusType()]);
 }
