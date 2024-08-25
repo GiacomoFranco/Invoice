b90f894e7c7721907ba2a076cc089af8b225e7aa
Santy <passiflora.freelance@gmail.com>
2024-08-25 01:00:48 -0500

    ✨ Adding summary and payments component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InvoiceSummaryComponent } from "./components/invoice-summary/invoice-summary.component";
+import { InvoicePaymentComponent } from "./components/invoice-payment/invoice-payment.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InvoiceSummaryComponent],
+  imports: [RouterOutlet, InvoiceSummaryComponent, InvoicePaymentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
