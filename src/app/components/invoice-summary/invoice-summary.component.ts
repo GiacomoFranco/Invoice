e95b2eaf7692fa01d921ececc6e7685b5f292dcd
Santy <passiflora.freelance@gmail.com>
2024-08-25 12:38:23 -0500

    🚧 Replacing content into invoice-summary component

--- src/app/components/invoice-summary/invoice-summary.component.ts
+++ src/app/components/invoice-summary/invoice-summary.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SummaryHeaderComponent } from "./summary-header/summary-header.component";
+import { PartiesInvolvedComponent } from "../../shared/parties-involved/parties-involved.component";
+import { QuotationFieldComponent } from "../../shared/quotation-field/quotation-field.component";
 
 @Component({
   selector: 'app-invoice-summary',
   standalone: true,
-  imports: [SummaryHeaderComponent],
+  imports: [SummaryHeaderComponent, PartiesInvolvedComponent, QuotationFieldComponent],
   templateUrl: './invoice-summary.component.html',
   styleUrl: './invoice-summary.component.scss'
 })
