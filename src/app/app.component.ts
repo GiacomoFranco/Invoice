d2a231ff5a4ce5f43db96ca40f2c62aa90b55604
Santy <passiflora.freelance@gmail.com>
2024-08-24 23:23:47 -0500

    ✨ Adding invoice-summary component and styling it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InvoiceSummaryComponent } from "./components/invoice-summary/invoice-summary.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InvoiceSummaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
