d34f73cdaea3cb48308c9d8f49252a71a7a55912
Santy <passiflora.freelance@gmail.com>
2024-08-25 00:59:59 -0500

    ✨ Adding summary component

--- src/app/components/invoice-summary/invoice-summary.component.ts
+++ src/app/components/invoice-summary/invoice-summary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SummaryHeaderComponent } from "./summary-header/summary-header.component";
 
 @Component({
   selector: 'app-invoice-summary',
   standalone: true,
-  imports: [],
+  imports: [SummaryHeaderComponent],
   templateUrl: './invoice-summary.component.html',
   styleUrl: './invoice-summary.component.scss'
 })
