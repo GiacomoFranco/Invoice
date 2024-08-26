de083be302f7ca536e7e3da3dc5410ebddd0deed
Santy <passiflora.freelance@gmail.com>
2024-08-26 03:28:10 -0500

    🚧 Table items functionality

--- src/app/shared/table-items/table-items.component.ts
+++ src/app/shared/table-items/table-items.component.ts
@@ -1,5 +1,8 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { TStatus } from '../../models/types/states.type';
+import { FormsModule } from '@angular/forms';
+import { TQuotationField } from '../../models/types/quotation-fields.type';
+import { InvoiceService } from '../../services/invoice.service';
 
 @Component({
   selector: 'app-table-items',
@@ -9,16 +12,42 @@ import { TStatus } from '../../models/types/states.type';
   styleUrl: './table-items.component.scss',
 })
 export class TableItemsComponent {
-  items = signal<any[]>([{ test: 1 }, { test: 1 }]);
+  constructor(private invoiceService: InvoiceService) {}
+
+  tableType = input<TQuotationField>('materials');
+  items = input<any[]>();
   status = input<TStatus>('Unpaid');
 
   addRow(): void {
-    this.items.update((items) => [...items, items.length + 1]);
+    this.tableType() === 'materials'
+      ? this.invoiceService.materials.update((items: any) => [
+          ...items,
+          { name: '---', price: 0, quantity: 0, total: 0 },
+        ])
+      : this.invoiceService.labors.update((items: any) => [
+          ...items,
+          { name: '---', price: 0, quantity: 0, total: 0 },
+        ]);
+  }
+
+  updateValue(target: any, index: any, key: string): void {
+    this.tableType() === 'materials'
+      ? this.invoiceService.materials.update((items) => items.map((item: any, i: any) => (i === index ? { ...item, [key]: target.value } : item)))
+      : this.invoiceService.labors.update((items) => items.map((item: any, i: any) => (i === index ? { ...item, [key]: target.value } : item)));
   }
 
   deleteRow(index: any): void {
-    console.log(index);
+    this.tableType() === 'materials'
+      ? this.invoiceService.materials.update((items) =>
+          items.filter((_: any, indexArr: any) => indexArr !== index)
+        )
+      : this.invoiceService.labors.update((items) =>
+          items.filter((_: any, indexArr: any) => indexArr !== index)
+        );
+  }
 
-    this.items.update((items) => items.filter((item, indexArr) => indexArr !== index));
+  totalSumatory(item: any) {
+    let sumatory = item.price * item.quantity;
+    return sumatory % 1 === 0 ? Math.round(sumatory) : sumatory.toFixed(2);
   }
 }
